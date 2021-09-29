de0c78b4ad16824cd087c1d5399bdd226e72b254
Sergio Serrano <sdserranog@gmail.com>
2021-09-29 01:35:59 -0400

    feat(Root): Add Header component

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Logo from 'design-system/atoms/brand'
 import styles from './header.module.css'
 
 function Header() {
-  return <header></header>
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Logo />
+      </div>
+    </header>
+  )
 }
 
 export default Header
