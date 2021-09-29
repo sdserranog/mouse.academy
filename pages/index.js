f4f9e188fa2b3e53ea107f05be16421df97f31d3
Sergio Serrano <sdserranog@gmail.com>
2021-09-29 01:41:03 -0400

    feat(Root): Add problem page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import getAllCategories from 'services/categories/getAllCategories'
+import getAllProblems from 'services/problems/getAllProblems'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ categories, problems }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +12,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        {categories.map(({ name, id }) => (
+          <span key={id}>{name}</span>
+        ))}
+        {problems.map(({ title, id, slug }) => (
+          <a key={id} href={`problem/${slug}`}>
+            {title}
+          </a>
+        ))}
+      </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  try {
+    const categories = await getAllCategories()
+    const problems = await getAllProblems()
+    return {
+      props: {
+        categories,
+        problems,
+      },
+    }
+  } catch (e) {
+    return {
+      notFound: true,
+    }
+  }
 }
--- pages/problem/[slug].js
+++ pages/problem/[slug].js
@@ -1,10 +1,53 @@
 import React from 'react'
+import Head from 'next/head'
+import styles from './problem.module.css'
+
 import getAllProblems from 'services/problems/getAllProblems'
 import getProblemBySlug from 'services/problems/getProblemBySlug'
+
+import Tabs from 'design-system/atoms/tabs'
+import Tab from 'design-system/atoms/tabs/tab/Tab'
+
 import Description from 'components/Problem/Description'
+import CodeEditor from 'components/Problem/CodeEditor'
+import Card from 'components/Problem/Card'
 
 const Problem = ({ problem }) => {
-  return <Description description={problem.description} />
+  return (
+    <>
+      <Head>
+        <title>Mouse Academy</title>
+        <meta name="description" content="Mouse Academy" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <Tabs>
+            <Tab title="Descripción">
+              <Description description={problem.description} />
+            </Tab>
+            <Tab title="Solución">
+              <CodeEditor />
+            </Tab>
+            <Tab title="Explicación">
+              <Description description={problem.description} />
+            </Tab>
+          </Tabs>
+        </div>
+        <div className={styles['code-editor']}>
+          <Card title="Solución">
+            <CodeEditor />
+          </Card>
+        </div>
+        <div className={styles.tests}>
+          <Card title="Tests">
+            <CodeEditor />
+          </Card>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default Problem
