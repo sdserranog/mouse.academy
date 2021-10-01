31d4d823851f847e888191f4ba965f7a34042c62
Sergio Serrano <sdserranog@gmail.com>
2021-10-01 12:18:17 -0400

    feat(Root): Add monaco editor background color

--- components/Problem/CodeEditor/editorOptions.js
+++ components/Problem/CodeEditor/editorOptions.js
@@ -2,6 +2,19 @@ const editorOptions = {
   minimap: {
     enabled: false,
   },
+  scrollBeyondLastLine: false,
+  scrollbar: {
+    useShadows: false,
+    verticalHasArrows: true,
+    horizontalHasArrows: true,
+    // set scrollbar hidden
+    vertical: 'hidden',
+    horizontal: 'hidden',
+
+    verticalScrollbarSize: 0,
+    horizontalScrollbarSize: 17,
+    arrowSize: 30,
+  },
 }
 
 export default editorOptions
